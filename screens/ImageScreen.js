5f8e4b8a6b2af3b42db82d129c47865703105ea2
TheMVProgrammer <franklin0415@hotmail.com>
2022-12-03 21:34:33 -0400

    Adding some components

---
+++ screens/ImageScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>Image Screen</Text>
+    </View>
+  )
+}
+
+export default ImageScreen
